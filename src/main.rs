901fd85f9feffe6a897380f0f6252205a4ce64ba
_rika <i@ksqsf.moe>
2018-12-18 15:21:53 +0800

    Make DNS resolver async

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use futures::prelude::*;
+use futures::future;
+use futures::sync::mpsc;
 use tokio::net::{UdpSocket, UdpFramed};
 use std::collections::HashMap;
 use std::net::SocketAddr;
@@ -10,6 +12,7 @@ extern crate log;
 mod message;
 mod codec;
 
+use crate::message::DnsMessage;
 use crate::codec::DnsMessageCodec;
 
 fn main() {
@@ -18,34 +21,31 @@ fn main() {
     let sock = UdpSocket::bind(&"0.0.0.0:1234".parse().unwrap()).unwrap();
     let records: Arc<Mutex<HashMap<u16, SocketAddr>>> = Arc::new(Mutex::new(HashMap::new()));
     let (udp_out, udp_in) = UdpFramed::new(sock, DnsMessageCodec::new()).split();
-    let mut udp_out = udp_out.wait();
+    let (tx, rx) = mpsc::unbounded::<(DnsMessage, SocketAddr)>();
+    let mut tx = tx.wait();
 
-    tokio::run(udp_in.for_each(move |(message, addr)| {
+    let sender = rx.fold(udp_out, |udp_out, msg| {
+        udp_out.send(msg)
+            .map_err(|e| error!("{}", e))
+    }).map_err(|e| error!("error in sender: {:?}", e));
+
+    let dispatcher = udp_in.for_each(move |(message, addr)| {
         let id = message.header.id;
         let dnsaddr: SocketAddr = "202.141.178.13:5353".parse().unwrap();
 
         if message.is_query() {
             debug!("Message {:x} from {} is query", id, addr);
-            match udp_out.send((message, dnsaddr)) {
-                Ok(_) => debug!("request proxied to dns server"),
-                Err(e) => error!("failed to send dgram to server: {}", e)
-            }
-            let mut records = records.lock().unwrap();
-            records.entry(id).and_modify(|e| *e = addr).or_insert(addr);
+            tx.send((message, dnsaddr));
+            records.lock().unwrap().entry(id).and_modify(|e| *e = addr).or_insert(addr);
         } else {
             debug!("Message {:x} from {} is response", id, addr);
             if let Some(client_addr) = records.lock().unwrap().remove(&id) {
-                match udp_out.send((message, client_addr)) {
-                    Ok(_) => debug!("sent to client {}", client_addr),
-                    Err(e) => error!("failed to send dgram to client {}: {}", client_addr, e)
-                }
+                tx.send((message, client_addr));
             }
         }
 
-        println!("{:?}", records);
-
         Ok(())
-    }).map_err(|err| {
-        println!("{:#?}", err);
-    }));
+    }).map_err(|e| error!("error in dispatcher: {}", e));
+
+    tokio::run(sender.join(dispatcher).map(|_| ()));
 }
